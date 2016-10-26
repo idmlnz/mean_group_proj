e079273287aa38d64033b6310ff19c264af02a9a
Ismelito Malonzo <idmlnz@yahoo.com>
2016-10-26 12:48:16 -0700

    Added files

---
+++ client/static/images/uploadKitten.sh
@@ -0,0 +1,28 @@
+#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  \
+#'{"name": "cat1", "age":4, "color":"white", "video":"video"}'  \
+#http://localhost:8000/kitten
+
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat1", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat2", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat3", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat4", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat5", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat6", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat7", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat8", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat9", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat10", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat11", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat12", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat13", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat14", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat15", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat16", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat17", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat18", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat19", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat20", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat21", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat22", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat23", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d  '{"name": "cat24", "age":4, "color":"white", "video":"video"}'    http://localhost:8000/kitten
